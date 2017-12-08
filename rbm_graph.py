ec70a6c2b16b28ce3f9173f18c57eba59985c045
Ned Ruggeri <ruggeri@self-loop.com>
2017-12-08 15:26:44 -0800

    further document calculation/choice of marginal_free_energy
    Also, I believe I fixed a bug by not using an axis for reduce_sum

--- rbm_graph.py
+++ rbm_graph.py
@@ -20,7 +20,40 @@ RBMGraph = namedtuple(
     ]
 )
 
+def marginal_free_energy(visible_units, W, visible_bias, hidden_bias):
+    # This approach calculates the *marginal* energy of the visible
+    # units, over all possible configurations of the hidden units. You
+    # don't need to explicitly sum over all `h`, which is nice,
+    # because of the simple structure of RBMs.
+    return (
+        -tf.reduce_sum(visible_units * visible_bias, axis = 1)
+        +
+        tf.reduce_sum(
+            tf.log_sigmoid(
+                -hidden_bias
+                -tf.matmul(visible_units, W)
+            ),
+            axis = 1
+        )
+    )
+
+def energy(visible_units, hidden_units, W, visible_bias, hidden_bias):
+    # This approach calculates the exact energy for an entire
+    # configuration of the machine.
+    return (
+        -tf.reduce_sum(visible_units * visible_bias, axis = 1)
+        -
+        tf.reduce_sum(
+            tf.matmul(visible_units, W)
+            *
+            hidden_units,
+            axis = 1
+        )
+        -tf.reduce_sum(hidden_units * hidden_bias, axis = 1)
+    )
+
 def make_rbm_graph():
+    # Parameters
     W = tf.Variable(
         tf.truncated_normal([
             config.NUM_VISIBLE_UNITS,
@@ -44,15 +77,11 @@ def make_rbm_graph():
         tf.float32, [None, config.NUM_HIDDEN_UNITS]
     )
 
-    energy_pos = (
-        -tf.reduce_sum(visible_units_pos * visible_bias, axis = 1)
-        +
-        tf.reduce_sum(
-            tf.log_sigmoid(
-                -hidden_bias
-                -tf.matmul(visible_units_pos, W)
-            )
-        )
+    energy_pos = marginal_free_energy(
+        visible_units = visible_units_pos,
+        W = W,
+        visible_bias = visible_bias,
+        hidden_bias = hidden_bias
     )
 
     # Negative side.
@@ -63,17 +92,14 @@ def make_rbm_graph():
         tf.float32, [None, config.NUM_HIDDEN_UNITS]
     )
 
-    energy_neg = (
-        -tf.reduce_sum(visible_units_neg * visible_bias, axis = 1)
-        +
-        tf.reduce_sum(
-            tf.log_sigmoid(
-                -hidden_bias
-                -tf.matmul(visible_units_neg, W)
-            )
-        )
+    energy_pos = marginal_free_energy(
+        visible_units = visible_units_neg,
+        W = W,
+        visible_bias = visible_bias,
+        hidden_bias = hidden_bias
     )
 
+    # This is our estimate of the gradient of p(v).
     energy_diff = tf.reduce_sum(energy_pos - energy_neg)
 
     learning_rate = tf.placeholder(tf.float32, name = 'learning_rate')
